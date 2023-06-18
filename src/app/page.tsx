51a21e87e3a835b255c4c1d4066b2d2a8b86296d
tetetratra <44367208+tetetratra@users.noreply.github.com>
2023-06-18 13:59:19 +0900

    枠組みはできた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,126 @@
+"use client"
+
 import Image from 'next/image'
+import classNames from 'classnames'
+import { useState, useLayoutEffect, ReactNode } from 'react'
+import _ from 'lodash'
 
-export default function Home() {
+const checkMd = () => window.matchMedia('(min-width:768px)').matches
+
+export default () => {
+  const [isMd, setIsMd] = useState(true)
+  useLayoutEffect(() => { // 参考: https://stackoverflow.com/questions/19014250/rerender-view-on-browser-resize-with-react
+    const updateIsMd = () => setIsMd(checkMd())
+
+    window.addEventListener('resize', updateIsMd)
+    updateIsMd()
+    return () => window.removeEventListener('resize', updateIsMd)
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="w-[360px] md:w-[750px] lg:w-[900px] m-auto mt-2">
+      <Row title>
+        <Item itemSize={ItemSize.Full} color='bg-[#DFFFDF]'>
+          tetetratra.net
+        </Item>
+      </Row>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Row>
+        <Item itemSize={ItemSize.Single} color='bg-[#DFFFFF]'>
+          link
+        </Item>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <Item itemSize={ItemSize.Double} color='bg-[#DFFFEF]'>
+          links...
+        </Item>
+      </Row>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {isMd ? (
+        <Row>
+          <Item itemSize={ItemSize.Double} color='bg-[#EFFFEF]'>
+            works...
+          </Item>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          <Item itemSize={ItemSize.Single} color='bg-[#FFFFDF]'>
+            work
+          </Item>
+        </Row>
+      ) : (
+        <Row>
+          <Item itemSize={ItemSize.Single} color='bg-[#FFFFDF]' placeRight>
+            work
+          </Item>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Item itemSize={ItemSize.Double} color='bg-[#EFFFEF]'>
+            works...
+          </Item>
+        </Row>
+      )}
+
+      <Row>
+        <Item itemSize={ItemSize.Single} color='bg-[#FFEFFF]'>
+          career
+        </Item>
+
+        <Item itemSize={ItemSize.Double} color='bg-[#EFFFDF]'>
+          careers...
+        </Item>
+      </Row>
+    </div>
   )
 }
+
+const Row = ({ title, children }: { title?: boolean, children?: ReactNode }) => {
+  const height = `
+       ${title ? 'h-[180px]' : 'h-[270px]'}
+    md:h-[250px]
+    lg:h-[300px]
+  `
+  const grid = `
+       grid grid-flow-col
+       grid-rows-3     grid-cols-2
+    md:grid-rows-1  md:grid-cols-3
+  `
+  return (
+    <div className={classNames('w-auto', height, grid)}>
+      {children}
+    </div>
+  )
+}
+
+enum ItemSize { Single, Double, Full }
+
+const Item = ({ itemSize, color, placeRight, children }: { itemSize: ItemSize, color: string, placeRight?: boolean, children?: ReactNode }) => {
+  let width = ''
+  switch (itemSize) {
+    case ItemSize.Single:
+      width = 'col-span-1 md:col-span-1' + (placeRight ? ' col-start-2' : '')
+      break;
+    case ItemSize.Double:
+      width = 'col-span-2 md:col-span-2'
+      break;
+    case ItemSize.Full:
+      width = 'col-span-2 md:col-span-3'
+      break;
+  }
+
+  let height = ''
+  switch (itemSize) {
+    case ItemSize.Single:
+      height = 'row-span-1 md:row-span-1'
+      break;
+    case ItemSize.Double:
+      height = 'row-span-2 md:row-span-1'
+      break;
+    case ItemSize.Full:
+      height = 'row-span-3 md:row-span-1'
+      break;
+  }
+
+  return (
+    <div className={classNames('rounded-2xl text-3xl m-1 md:m-2', width, height, color)}>
+      {children}
+    </div>
+  )
+}
+
